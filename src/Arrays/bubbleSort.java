3f7faec3b70156e690b3f11aa365668dde23037b
Divyanshu Singh <divyanshusingh6747@gmail.com>
2022-01-25 20:17:58 +0530

    Bubble Sort

--- src/Arrays/bubbleSort.java
+++ src/Arrays/bubbleSort.java
@@ -1,43 +1,39 @@
 package Arrays;
 import java.util.Scanner;
-class bubble
-{
-    private int []array;
+class bubble {
+    private int[] array;
     private int size;
-    public void getArray()
-    {
-        Scanner sc=new Scanner(System.in);
+
+    public void getArray() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
-        size=sc.nextInt();
-        array=new int[size];
+        size = sc.nextInt();
+        array = new int[size];
         System.out.println("Enter the element in the array");
         for (int i = 0; i < size; i++) {
-            array[i]= sc.nextInt();
+            array[i] = sc.nextInt();
         }
     }
 
-    public void BubbleSort()
-    {
-        for (int i = size-1; i >1 ; i--) {
-            for (int j = 0; j <i ; j++) {
-                if(array[j]>array[j+1])
-                {
-                   int temp=array[j];
-                   array[j]=array[j+1];
-                   array[j+1]=temp;
+    public void BubbleSort() {
+        for (int i = size - 1; i > 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
-                System.out.print(array[j]+" ");
             }
-            System.out.println();
+            System.out.println("After the Bubble sort the array is :");
+            for (i = 0; i < size; i++) {
+                System.out.print(array[i] + " ");
+            }
         }
-//        System.out.println("After the Bubble sort the array is :");
-//        for (int i = 0; i < size; i++) {
-//            System.out.print(array[i]+" ");
-//        }
+
     }
 
 }
-public class bubbleSort {
+public  class bubbleSort {
     public static void main(String[] args) {
         bubble b=new bubble();
         b.getArray();
