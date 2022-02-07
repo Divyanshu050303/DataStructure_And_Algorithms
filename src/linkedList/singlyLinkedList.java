686b253b519392bb1552bdfe0d3add0bd61bfa6e
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-07 17:09:30 +0530

    Singly Linked List All Basic Operation

--- src/linkedList/singlyLinkedList.java
+++ src/linkedList/singlyLinkedList.java
@@ -169,7 +169,6 @@ public class singlyLinkedList {
                     ll.insertFirst(value);
                     break;
                 }
-
                 case 2: {
                     System.out.println("Enter the value which you want to insert");
                     int value = sc.nextInt();
