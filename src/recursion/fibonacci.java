113855b935eeea20403c0fb1f39c236950a79774
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-08 16:50:21 +0530

    Fibonacci series

---
+++ src/recursion/fibonacci.java
@@ -0,0 +1,18 @@
+package recursion;
+class fibo
+{
+    public int fiboo(int n)
+    {
+        if(n<2)
+        {
+            return n;
+        }
+        return fiboo(n-1)+fiboo(n-2);
+    }
+}
+public class fibonacci {
+    public static void main(String[] args) {
+        fibo f=new fibo();
+        System.out.println(f.fiboo(8));
+    }
+}
