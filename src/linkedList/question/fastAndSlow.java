f64707ebed6ac1fc5e6f8b2443b398c7111d502b
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-17 22:31:58 +0530

    Fast and slow

---
+++ src/linkedList/question/fastAndSlow.java
@@ -0,0 +1,20 @@
+package linkedList.question;
+//https://leetcode.com/problems/linked-list-cycle/submissions/
+public class fastAndSlow {
+
+//    public boolean hasCycle(ListNode head) {
+//        ListNode slow=head;
+//        ListNode fast=head;
+//        while(fast!=null && fast.next!=null)
+//        {
+//            fast=fast.next.next;
+//            slow=slow.next;
+//            if(fast==slow)
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+//
+//    }
+}
