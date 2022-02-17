a12d6e0ee30173036330df4372cc3d8a215e01cd
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-17 23:17:22 +0530

    Happy Number

---
+++ src/linkedList/happyNumber.java
@@ -0,0 +1,35 @@
+package linkedList;
+//https://leetcode.com/problems/happy-number/
+class Number
+{
+    private int square(int num)
+    {
+        int ans=0;
+        while (num>0)
+        {
+            int rem=num%10;
+            ans+=rem*rem;
+            num/=10;
+        }
+        return ans;
+    }
+    public boolean isHappy(int n)
+    {
+        int slow=n;
+        int fast=n;
+        do {
+            slow=square(slow);
+            fast=square(square(fast));
+        }while (slow!=fast);
+        if(slow==1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
+public class happyNumber {
+
+
+}
