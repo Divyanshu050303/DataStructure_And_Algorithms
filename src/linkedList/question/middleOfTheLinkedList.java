431e335fa231f52f04c748ab137ac58bd1a194af
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-18 21:20:48 +0530

    middleOfTheLinkedList

---
+++ src/linkedList/question/middleOfTheLinkedList.java
@@ -0,0 +1,18 @@
+package linkedList.question;
+//https://leetcode.com/problems/middle-of-the-linked-list/
+class linked
+{
+//    public ListNode middleNode(ListNode head) {
+//        ListNode slow=head;
+//        ListNode fast=head;
+//        while(fast!=null && fast.next!=null)
+//        {
+//            slow=slow.next;
+//            fast=fast.next.next;
+//        }
+//        return slow;
+//
+//    }
+}
+public class middleOfTheLinkedList {
+}
