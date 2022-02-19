66ddfa56108c38a939962ce7715aab33fcf18df3
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-02-19 11:36:12 +0530

    Stack implementation by using Linked List

---
+++ src/Stacks/stackImplementationByLinkedList.java
@@ -0,0 +1,104 @@
+package Stacks;
+
+import java.util.Scanner;
+
+class implement
+{
+    Node head;
+    class Node
+    {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    public void insert(int data)
+    {
+        Node temp=new Node(data);
+        temp.next=head;
+        head=temp;
+        System.out.println("Data inserted.....");
+    }
+    public void delete()
+    {
+        if(head==null)
+        {
+            System.out.println("List is empty");
+        }
+        else
+        {
+            int data=head.value;
+            head=head.next;
+            System.out.println(data+" is deleted from the list");
+        }
+    }
+    public void traverse()
+    {
+        if(head==null)
+        {
+            System.out.println("List is empty");
+        }
+        else
+        {
+            Node temp=head;
+            while (temp!=null)
+            {
+                System.out.print(temp.value+"->");
+                temp=temp.next;
+            }
+            System.out.println("end");
+        }
+    }
+}
+
+public class stackImplementationByLinkedList {
+    public static void main(String[] args) {
+        implement i=new implement();
+        System.out.println("Press 1 -> insert");
+        System.out.println("Press 1 -> Delete");
+        System.out.println("Press 1 -> traverse");
+        System.out.println("Press 1 -> exit");
+        while (true)
+        {
+            System.out.println("Enter the choice");
+            Scanner sc=new Scanner(System.in);
+            int choice=sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                {
+                    System.out.println("Enter the data ");
+                    int data=sc.nextInt();
+                 i.insert(data);
+                 break;
+                }
+                case 2:
+                {
+                    i.delete();
+                    break;
+
+                }
+                case 3:
+                {
+                    i.traverse();
+                    break;
+
+                }
+                case 4:
+                {
+                    System.exit(0);
+                    break;
+
+                }
+                default:
+                {
+                    System.out.println("Wrong choice");
+                    break;
+
+                }
+            }
+        }
+    }
+}
