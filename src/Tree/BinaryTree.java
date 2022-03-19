d49e8dd4e9a6016a3b60cbad736ebcbfcf77c3e8
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-03-19 11:20:40 +0530

    Binary tree

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -2,6 +2,7 @@ package Tree;
 import java.util.Scanner;
 import java.util.*;
 class binary {
+    // This is the binary Node class
     static class Node {
         int data;
         Node leftChild;
@@ -15,7 +16,7 @@ class binary {
     }
 
     static int idx = -1;
-
+    // to Build the binary tree
     public Node buildTree(int[] node) {
         idx++;
         if (node[idx] == -1) {
@@ -26,6 +27,7 @@ class binary {
         newNode.rightChild = buildTree(node);
         return newNode;
     }
+    // To print in preorder
     public void preOrder(Node root){
         if(root==null){
             return;
@@ -34,6 +36,7 @@ class binary {
         preOrder(root.leftChild);
         preOrder(root.rightChild);
     }
+    // to print in  inorder
     public void inOrder(Node root){
         if(root==null){
             return;
@@ -42,6 +45,7 @@ class binary {
         System.out.print(root.data+" ");
         inOrder(root.rightChild);
     }
+    // to print in postorder
     public void postOrder(Node root){
         if(root==null){
             return;
@@ -50,6 +54,7 @@ class binary {
         postOrder(root.rightChild);
         System.out.print(root.data+" ");
     }
+    // to print in level order
     public void levelOrder(Node root){
         if(root==null){
             return;
@@ -79,6 +84,7 @@ class binary {
             }
         }
     }
+    // to count the number of node in the tree
     public int countOfNode(Node root){
         if(root==null){
             return 0;
@@ -87,6 +93,7 @@ class binary {
         int rightCount=countOfNode(root.rightChild);
         return leftCount+rightCount+1;
     }
+    // to calculate the sum of the node
     public int sumOfNode(Node root){
         if(root==null){
             return 0;
@@ -95,21 +102,18 @@ class binary {
         int rightSUm=sumOfNode(root.rightChild);
         return leftSum+rightSUm+root.data;
     }
+    // to calculate the height of the tree
     public int height(Node root){
         if(root==null){
             return 0;
         }
         int leftHeight=height(root.leftChild);
         int rightHeight=height(root.rightChild);
-        int height;
-        if(leftHeight<rightHeight){
-            height=rightHeight;
-        }else {
-           height=leftHeight;
-        }
+        int height = Math.max(leftHeight, rightHeight);
 
         return height+1;
     }
+    // to calculate the diameter of the node
     public int diameter(Node root){
         if(root==null){
             return 0;
@@ -129,7 +133,8 @@ class binary {
         }
         return maximumDiameter;
     }
-    class treeInformation{
+    // to calculate the diameter of the tree we create a reference
+    static class treeInformation{
         int height, diameter;
         treeInformation(int height, int diameter){
             this.diameter=diameter;
@@ -151,9 +156,10 @@ class binary {
         int rootDiameter=leftDiameter+rightDiameter+1;
 
         int myDiameter=Math.max(Math.max(leftDiameter, rightDiameter), rootDiameter);
-        treeInformation treeinfo=new treeInformation(myHeight, myDiameter);
+        treeInformation treeinfo= new treeInformation(myHeight, myDiameter);
         return treeinfo;
     }
+    // to calculate the minimum node from the tree
     public Node Minimum(Node root){
         Node current;
         Node last = null;
@@ -163,6 +169,54 @@ class binary {
             current=current.leftChild;
         }
         return last;
+    }
+    // to print in inorder in Morris form
+    public void inOrderMorris(Node root){
+        Node current=root;
+        while(current!=null){
+            if(current.leftChild==null){
+                System.out.print(current.data+" ");
+                current=current.rightChild;
+            }
+            else{
+                Node predecessor=current.leftChild;
+                while(predecessor.rightChild!=current && predecessor.rightChild!=null){
+                    predecessor=predecessor.rightChild;
+                }
+                if(predecessor.rightChild==null){
+                    predecessor.rightChild=current;
+                    current=current.leftChild;
+                }else{
+                    predecessor.rightChild=null;
+                    System.out.print(current.data+" ");
+                    current=current.rightChild;
+                }
+            }
+        }
+    }
+    // to print in preOrder in Morris form
+    public void  preOrderMorris(Node root){
+        Node current=root;
+        while (current!=null){
+            if(current!=null){
+                System.out.print(current.data+" ");
+                current=current.rightChild;
+            }
+            else {
+                Node predecessor=current.leftChild;
+                while (predecessor.rightChild!=current && predecessor.rightChild!=null){
+                    predecessor=predecessor.rightChild;
+                }
+                if(predecessor.rightChild==null){
+                    predecessor.rightChild=current;
+                    System.out.print(current.data+" ");
+                    current=current.leftChild;
+                }else{
+                    predecessor.rightChild=null;
+                    current=current.rightChild;
+                }
+            }
+        }
     }
 
 }
@@ -184,7 +238,9 @@ public class BinaryTree {
             System.out.println("Press 8 -> Diameter of the tree Approach 1");
             System.out.println("Press 9 -> Diameter of the tree Approach 2");
             System.out.println("Press 10 -> Find the Minimum value in the tree");
-            System.out.println("Press 11 -> exit");
+            System.out.println("Press 11 -> Find the inOrderMorris traversal");
+            System.out.println("Press 12 -> find the preOrderMorris traversal");
+            System.out.println("Press 13 -> exit");
             System.out.println();
             System.out.println("Enter you choice");
             int choice=sc.nextInt();
@@ -212,6 +268,9 @@ public class BinaryTree {
                 case 5:{
                     System.out.println("Count the node of the tree");
                     System.out.println(tree.countOfNode(root));
+
+
+
                     break;
                 }
                 case 6:{
@@ -237,8 +296,19 @@ public class BinaryTree {
                 case 10:{
                     System.out.println("Minimum value in the tree");
                     System.out.println(tree.Minimum(root).data);
+                    break;
                 }
                 case 11:{
+                    System.out.println("inOrderMorris traversal is :");
+                    tree.inOrderMorris(root);
+                    break;
+                }
+                case 12:{
+                    System.out.println("preOrderMorris traversal is :");
+                    tree.preOrderMorris(root);
+                    break;
+                }
+                case 13:{
                     System.exit(0);
                 }
             }
