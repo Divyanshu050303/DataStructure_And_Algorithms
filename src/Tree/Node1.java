eaa6174234e6c0946793ce12e0995fd13ed4ca06
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-03-26 22:46:17 +0530

    Node class

---
+++ src/Tree/Node1.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Node1 {
+    Node1 leftChild;
+    Node1 rightChild;
+    int data;
+
+    Node1(int data) {
+        this.data = data;
+        this.rightChild = null;
+        this.leftChild=null;
+    }
+}
