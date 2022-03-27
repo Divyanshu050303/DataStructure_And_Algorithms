9db4c7bab7084decc99ebcdc950dae8fd3b0394b
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-03-27 14:55:06 +0530

    sum Of Node

---
+++ src/Tree/sumOfNode.java
@@ -0,0 +1,19 @@
+package Tree;
+
+public class sumOfNode {
+    public int sum(binary.Node root){
+        if(root==null){
+            return 0;
+        }
+        int sumOfLeftChild=sum(root.leftChild);
+        int sumOfRightChild=sum(root.rightChild);
+        return sumOfLeftChild+sumOfRightChild+1;
+    }
+
+    public static void main(String[] args) {
+        sumOfNode Tree=new sumOfNode();
+        int []node={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        binary.Node root=binary.buildTree(node);
+        System.out.println(Tree.sum(root));
+    }
+}
