0551df70782308ee035fbf8c06da65f4d46cafc7
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-04-02 23:41:50 +0530

    Find the kth maximum and minimum number from the array

---
+++ src/Arrays/question/findTheKthMaxAndMin.java
@@ -0,0 +1,47 @@
+package Arrays.question;
+import java.util.Scanner;
+class find2{
+    int []arr;
+    int size, max, min;
+    public void insert1(){
+        System.out.println("Enter the size of the array");
+        Scanner sc=new Scanner(System.in);
+        size=sc.nextInt();
+        arr=new int[size];
+        System.out.println("Enter the element in the array");
+        for (int i = 0; i <size ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Which maximum number do you want");
+        max=sc.nextInt();
+        System.out.println("Which minimum number do you want");
+        min=sc.nextInt();
+        traversal();
+        maxMin2();
+    }
+    public void traversal(){
+        System.out.println("Array which has you entered");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public void maxMin2(){
+        for (int i=0;i<size;i++){
+            for (int j = 0; j < size-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Maximum number whichever you want "+arr[size-max]);
+        System.out.println("Minimum number whichever you want "+arr[min-1]);
+    }
+}
+public class findTheKthMaxAndMin {
+    public static void main(String[] args) {
+        find2 f=new find2();
+        f.insert1();
+    }
+}
