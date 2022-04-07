b639b2ab6a81d66a9c477f1e871172cb60feec01
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-04-07 22:59:11 +0530

    Last Node Of The LinkedList

---
+++ src/linkedList/question/lastNodeOfTheLinkedList.java
@@ -0,0 +1,63 @@
+package linkedList.question;
+class last{
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    Node head;
+    public void insert(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node tem=head;
+        while (tem.next!=null){
+            tem=tem.next;
+        }
+        tem.next=newNode;
+    }
+    public void traversal(){
+        Node temp=head;
+        while (temp != null) {
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public void fastSlow(){
+        Node fast=head;
+        Node slow=head;
+        while (fast.next!=null ){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+//        Node tem=head;
+//        while (tem.next!=slow){
+//            tem=tem.next;
+//        }
+//        tem.next=slow.next;
+//        System.out.println("After deletion the middle node");
+//        traversal();
+        System.out.println("The last node of the linked List is "+fast.data);
+    }
+}
+public class lastNodeOfTheLinkedList {
+    public static void main(String[] args) {
+        last l=new last();
+        l.insert(1);
+        l.insert(2);
+        l.insert(3);
+        l.insert(4);
+        l.insert(5);
+        l.insert(6);
+        l.insert(7);
+        System.out.println("Actual LinkedList");
+        l.traversal();
+        l.fastSlow();
+    }
+}
