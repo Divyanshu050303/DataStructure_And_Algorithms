5ddb6ab65e77b32613b5f4bd97475bc2ecf47f1f
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-04-10 17:07:53 +0530

    BFS Of The Graph

--- src/graph/bfsOfTheGraph.java
+++ src/graph/bfsOfTheGraph.java
@@ -1,5 +1,19 @@
 package graph;
 import java.util.*;
+/*Breadth first search is a traversal technique in which we traverse all the node of the graph in a
+breadth-wise motion . In BFS, we traverse one level at a time and then jump to the next level .In a
+graph, the traversal can start from any node and cover all the nodes level-wise
+
+time complexity is O(V+E)
+where V is a number of vertices in the graph ad E is a number of edges in the graph.
+
+Application of the breadth first search
+1->Peer To Peer Networks
+2->Broadcasting in NetWork
+3->Finding all node within one connected component
+4->Path Finding
+5->GPS Navigation
+ */
 public class bfsOfTheGraph {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>>adj, int v){
         ArrayList<Integer> bfs=new ArrayList<>();
