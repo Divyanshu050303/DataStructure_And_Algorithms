755259527471b824676acef06f080a93ba132d1e
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-04-13 22:47:42 +0530

    class

--- src/Sorting/array/selectionSort.java
+++ src/Sorting/array/selectionSort.java
@@ -27,7 +27,7 @@ class selection{
     }
     public void sort()
     {
-        for (int i = 0; i <size ; i++) {
+        for (int i = 0; i <size-1 ; i++) {
             int min=i;
             for (int j = i+1; j <size ; j++) {
                 if(array[j]<array[min]){
