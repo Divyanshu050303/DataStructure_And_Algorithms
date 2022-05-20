896a692f7d1940856e06cdd968980b25649dc2de
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-05-20 21:46:49 +0530

    print all the subsequence of the string

---
+++ src/string/PrintAllSubsequencesOfAString.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.Scanner;
+
+class print{
+    String str;
+    public void insert(){
+        System.out.println("Enter the string");
+        Scanner sc=new Scanner(System.in);
+        str=sc.next();
+
+    }
+}
+public class PrintAllSubsequencesOfAString {
+}
