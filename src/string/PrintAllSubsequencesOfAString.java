3e0d65e3c2abe66be3db14139b7aa917da14bc75
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-05-20 21:50:39 +0530

    print all the subsequence of the string

--- src/string/PrintAllSubsequencesOfAString.java
+++ src/string/PrintAllSubsequencesOfAString.java
@@ -8,8 +8,25 @@ class print{
         System.out.println("Enter the string");
         Scanner sc=new Scanner(System.in);
         str=sc.next();
+        string("", str);
+
+    }
+    public void string(String p, String up)
+    {
+        if(up.isEmpty())
+        {
+            System.out.print(p+" ");
+            return;
+        }
+        char ch=up.charAt(0);
+        string(p, up.substring(1));
+        string(p+ch, up.substring(1));
 
     }
 }
 public class PrintAllSubsequencesOfAString {
+    public static void main(String[] args) {
+        print p=new print();
+        p.insert();
+    }
 }
