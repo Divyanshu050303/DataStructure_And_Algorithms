ced773dca1fb251690e706fd75e33abe9f7ce707
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-05-25 17:09:02 +0530

    swapping the number through bit manipulation

---
+++ src/BitManipulation/swapingTwoNumber.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+public class swapingTwoNumber {
+    public static void main(String[] args) {
+          int a, b;
+          System.out.println("Enter the first number");
+          Scanner sc=new Scanner(System.in);
+          a=sc.nextInt();
+          System.out.println("Enter the second number");
+          b= sc.nextInt();
+          System.out.println("Value of a and b before swapping "+a+" "+b);
+          a=a^b;
+          b=a^b;
+          a=a^b;
+        System.out.println("Value of a and b after swapping "+a+" "+b);
+
+    }
+}
