4813b95d5541a540db2352ed826b1adb6488d452
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-05-27 17:04:18 +0530

    Power of two

---
+++ src/BitManipulation/PowerOfTwo.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+class power{
+    int n;
+    public void two(){
+        System.out.println("Enter the number ");
+        Scanner sc=new Scanner(System.in);
+    }
+}
+public class PowerOfTwo {
+}
