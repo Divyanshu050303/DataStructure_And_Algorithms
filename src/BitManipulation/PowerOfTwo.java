5c3151c269421291acb5dc53eef6f6a0763b31fd
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-05-27 17:25:08 +0530

    Power of two

--- src/BitManipulation/PowerOfTwo.java
+++ src/BitManipulation/PowerOfTwo.java
@@ -4,10 +4,26 @@ import java.util.Scanner;
 
 class power{
     int n;
-    public void two(){
+    public boolean two(){
         System.out.println("Enter the number ");
         Scanner sc=new Scanner(System.in);
+        int s= sc.nextInt();
+
+        while (n>1 && n%2==0){
+            n/=2;
+        }
+
+        if(n==1.0){
+            return true;
+        }
+
+        return false;
+
     }
 }
 public class PowerOfTwo {
+    public static void main(String[] args) {
+        power p=new power();
+        System.out.println(p.two());
+    }
 }
