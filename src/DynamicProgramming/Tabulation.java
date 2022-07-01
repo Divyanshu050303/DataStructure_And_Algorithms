76bac4039a5cd0b680e8a7cd1cff3e2a44ac7e98
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-07-01 11:28:58 +0530

    tabulation

--- src/DynamicProgramming/Tabulation.java
+++ src/DynamicProgramming/Tabulation.java
@@ -2,10 +2,10 @@ package DynamicProgramming;
 import java.util.*;
 public class Tabulation {
     public static void main(String[] args) {
-        int n=5;
+        int n=3;
         int []fiboArray=new int[n+1];
         Arrays.fill(fiboArray, -1);
-        fiboArray[0]=0;
+        fiboArray[0]=1;
         fiboArray[1]=1;
         for (int i = 2; i <=n ; i++) {
             fiboArray[i]=fiboArray[i-1]+fiboArray[i-2];
