3613caed2af795b8081c18b35004291c30e1cf05
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-07-19 21:19:43 +0530

    class

--- src/recursion/permutation.java
+++ src/recursion/permutation.java
@@ -1,17 +1,21 @@
 package recursion;
+
+import java.util.ArrayList;
+
 class per
 {
+    ArrayList<String> arrayList=new ArrayList<>();
     public void mu(String p, String up)
     {
         if(up.isEmpty())
         {
-            System.out.print(p+" ");
+            arrayList.add(p+"");
             return;
         }
         char ch=up.charAt(0);
         for (int i = 0; i <= p.length(); i++) {
             String f=p.substring(0,i);
-            String s=p.substring(i, p.length());
+            String s=p.substring(i);
             mu(f+ch+s, up.substring(1));
         }
     }
@@ -19,6 +23,12 @@ class per
 public class permutation {
     public static void main(String[] args) {
         per p=new per();
-        p.mu("", "abc");
+        int k=3;
+        String l="";
+        for(int i=1;i<=k;i++){
+            l+=i;
+        }
+        p.mu("", l);
+        System.out.println(p.arrayList);
     }
 }
