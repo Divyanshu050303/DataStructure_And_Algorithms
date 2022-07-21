41fea7f061f79769997bdaef33775ba402a68145
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-07-21 08:57:12 +0530

    subString

--- src/recursion/subString.java
+++ src/recursion/subString.java
@@ -1,11 +1,16 @@
 package recursion;
+
+import java.util.ArrayList;
+
 class sub
 {
+    ArrayList<String> arrayList=new ArrayList<>();
     public void string(String p, String up)
     {
         if(up.isEmpty())
         {
-            System.out.print(p+" ");
+//            System.out.print(p+" ");
+            arrayList.add(p);
             return;
         }
         char ch=up.charAt(0);
@@ -17,6 +22,8 @@ class sub
 public class subString {
     public static void main(String[] args) {
         sub s=new sub();
-        s.string("", "abc");
+        s.string("", "abcde");
+        System.out.println(s.arrayList);
+
     }
 }
