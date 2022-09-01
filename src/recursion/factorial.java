3cf03717949ae7eccdaace2323b6bc66587efbe9
Divysnshu050303 <divyanshusingh02@outlook.com>
2022-09-01 22:03:10 +0530

    class

--- src/recursion/factorial.java
+++ src/recursion/factorial.java
@@ -1,4 +1,7 @@
 package recursion;
+
+import java.util.Scanner;
+
 class fact
 {
     public int facto(int n)
@@ -12,7 +15,16 @@ class fact
 }
 public class factorial {
     public static void main(String[] args) {
-        fact f=new fact();
-        System.out.println(f.facto(5));
+//        fact f=new fact();
+//        System.out.println(f.facto(5));
+        int fac=1;
+        System.out.println("Enter the number ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            fac=fac*i;
+        }
+        System.out.println(fac);
+
     }
 }
