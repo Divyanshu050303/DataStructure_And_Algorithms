1c8d62c3a99193abd69122aed51e7657465b00d1
Divysnshu050303 <divyanshusingh02@outlook.com>
2023-01-03 21:54:02 +0530

    counting sort

--- src/Sorting/array/counting.java
+++ src/Sorting/array/counting.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class counting {
     public static void main(String[] args) {
-        System.out.println("Enter the size of the array");
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int []a=new int[size];
+//        System.out.println("Enter the size of the array");
+//        Scanner sc=new Scanner(System.in);
+        int size=8;
+        int []a={2,5,3,0,2,3,0,3};
         int[]b;
 //
-        for (int i = 0; i < size; i++) {
-            a[i]=sc.nextInt();
-        }
+//        for (int i = 0; i < size; i++) {
+//            a[i]=sc.nextInt();
+//        }
         int t=a[0];
         for (int i = 0; i < size; i++) {
             if(t<a[i]){
@@ -23,11 +23,12 @@ public class counting {
         System.out.println(t);
         int []c=new int[t];
         b=new int[size];
-        for (int j = 0; j <t ; j++) {
+        for (int j = 1; j <t ; j++) {
+            c[j]=0;
             for (int i = 1; i <size ; i++) {
                 c[a[i]]=c[a[i]]+1;
             }
-            for ( j = 1; j < t; j++) {
+            for ( j = 2; j < t; j++) {
                 c[j]=c[j]+c[j-1];
             }
             for (int i = size-1; i >=1 ; i--) {
