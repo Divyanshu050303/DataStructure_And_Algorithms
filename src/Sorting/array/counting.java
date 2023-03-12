154bf6f6771400b48d0f0038b7ed81b45fb6c771
Divysnshu050303 <divyanshusingh02@outlook.com>
2023-03-12 11:24:51 +0530

    counting sort

--- src/Sorting/array/counting.java
+++ src/Sorting/array/counting.java
@@ -1,43 +1,38 @@
 package Sorting.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
+class time{
+    public void countingSort(int []arr){
+        int n=arr.length;
+        int []b=new int[n];
+        int k=arr[0];
+        for (int j : arr) {
+            if (k < j) {
+                k = j;
+            }
+        }
+        int []count=new int[k];
+        Arrays.fill(count,0);
+        for (int i = 0; i <k ; i++) {
+            count[arr[i]]=count[arr[i]]+1;
+        }
+        for (int i = 1; i <k ; i++) {
+            count[i]+=count[i-1];
+        }
+        for (int i = n-1; i >=1 ; i--) {
+            b[count[arr[i]]]=arr[i];
+            count[arr[i]]=count[arr[i]]-1;
+        }
+    }
+}
 public class counting {
     public static void main(String[] args) {
-//        System.out.println("Enter the size of the array");
-//        Scanner sc=new Scanner(System.in);
-        int size=8;
-        int []a={2,5,3,0,2,3,0,3};
-        int[]b;
-//
-//        for (int i = 0; i < size; i++) {
-//            a[i]=sc.nextInt();
-//        }
-        int t=a[0];
-        for (int i = 0; i < size; i++) {
-            if(t<a[i]){
-                t=a[i];
-            }
-        }
-        t=t+1;
-        System.out.println(t);
-        int []c=new int[t];
-        b=new int[size];
-        for (int j = 1; j <t ; j++) {
-            c[j]=0;
-            for (int i = 1; i <size ; i++) {
-                c[a[i]]=c[a[i]]+1;
-            }
-            for ( j = 2; j < t; j++) {
-                c[j]=c[j]+c[j-1];
-            }
-            for (int i = size-1; i >=1 ; i--) {
-                b[c[a[i]]]=a[i];
-                c[a[i]]=c[a[i]]-1;
-            }
-        }
-        for (int i = 0; i < size; i++) {
-            System.out.print(b[i]+" ");
+        int []arr={170, 45, 75, 96, 802, 24, 2, 66};
+        time t=new time();
+        t.countingSort(arr);
+        for(int i:arr){
+            System.out.println(i);
         }
     }
 }
