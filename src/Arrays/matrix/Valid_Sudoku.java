19b721af344ef797e23f05a619ae760a249aca42
Divysnshu050303 <divyanshusingh02@outlook.com>
2023-03-28 21:24:37 +0530

    Valid_Sudoku

---
+++ src/Arrays/matrix/Valid_Sudoku.java
@@ -0,0 +1,28 @@
+package Arrays.matrix;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Valid_Sudoku {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen=new HashSet();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char number=board[i][j];
+                if(number!='.'){
+                    if(seen.contains(number + "_R_" + i)||
+                            seen.contains(number + "_C_" + j)||
+                            seen.contains(number + "_B_" + i/3 + "_" + j/3)){
+                        return false;
+                    }
+                    else {
+                        seen.add(number + "_R_" + i);
+                        seen.add(number + "_C_" + j);
+                        seen.add(number + "_B_" + i/3 + "_" + j/3);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
