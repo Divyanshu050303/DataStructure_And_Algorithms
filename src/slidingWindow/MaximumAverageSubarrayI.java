bb2b9c84b96ea9f83d0dbaaf1ce0c053f79ed5e1
Divysnshu050303 <divyanshusingh02@outlook.com>
2023-05-16 21:36:33 +0530

    MaximumAverageSubarrayI

---
+++ src/slidingWindow/MaximumAverageSubarrayI.java
@@ -0,0 +1,37 @@
+package slidingWindow;
+
+public class MaximumAverageSubarrayI {
+    public static void main(String[] args) {
+//        int []arr={1,12,-5,-6,50,3};
+//        int k=4;
+//
+//        double max=Integer.MIN_VALUE;
+//        for(int i=0;i<arr.length-k;i++){
+//            int sum=0;
+//            for(int j=0;j<k;j++){
+//                sum+=arr[i+j];
+//            }
+//            max=Math.max(max, sum);
+//
+//        }
+//        System.out.println(max/
+
+
+
+//        double max=Integer.MIN_VALUE;
+//        double sum=0, sum1=0;
+//        int j=0;
+//        for(int i=0;i<arr.length;i++){
+//            sum+=arr[i];
+//            if(i>=(k-1)){
+//                sum1=sum/k;
+//                max=Math.max(max, sum1);
+//                sum-=arr[j];
+//                j++;
+//            }
+//
+//
+//        }
+//        return max;
+    }
+}
